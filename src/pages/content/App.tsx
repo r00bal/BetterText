6ff972c73b191224b75cccbe6e1a4cfa39991570
r00bal <andrzejewski.piotr@gmail.com>
2024-09-26 22:56:04 +0200

    feat: add dragable

--- src/pages/content/App.tsx
+++ src/pages/content/App.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import Drawer from "./Drawer";
-import Card from "./Card";
-import { LoaderSparkles } from "./LoaderSparkles";
+import React, { useEffect, useState } from "react";
 import { useBackgroundComunication } from "./hoooks/useBackgroundComunication";
 import { TypewriterEffect } from "@src/components/ui/typewriter-effect";
 import { Collapse } from "./Collapse";
 import { Loader } from "./Loader";
-import { motion } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 
 type ExplenationList = {
   original: string;
@@ -30,6 +27,37 @@ const App = () => {
   } = useBackgroundComunication();
 
   const { improved, explanation = [] } = improvedText || {};
+  const controls = useDragControls();
+
+  const [constraints, setConstraints] = useState({
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  });
+
+  useEffect(() => {
+    const updateConstraints = () => {
+      const cardElement = document.querySelector(".card");
+      if (cardElement) {
+        const rect = cardElement.getBoundingClientRect();
+        setConstraints({
+          left: -rect.left,
+          right: window.innerWidth - rect.right,
+          top: -rect.top,
+          bottom: window.innerHeight - rect.bottom,
+        });
+      }
+    };
+
+    updateConstraints();
+    window.addEventListener("resize", updateConstraints);
+    return () => window.removeEventListener("resize", updateConstraints);
+  }, [isOpen]);
+
+  function startDrag(event) {
+    controls.start(event);
+  }
 
   return (
     <>
@@ -37,87 +65,105 @@ const App = () => {
         OPEN BETTER ENGLISH
       </button>
       {isOpen && (
-        <motion.div
-          drag
-          className="card bg-base-100 w-96 shadow-xl fixed top-[50px] right-[50px] z-[100000]"
-        >
-          <div className="card-body p-2 rounded-sm">
-            <div className="card-actions justify-end">
-              <button
-                className="btn btn-outline btn-secondary"
-                onClick={() => {
-                  setIsOpen(false);
-                  setSelectedText("");
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+        <>
+          <motion.div
+            drag
+            dragControls={controls}
+            dragConstraints={constraints}
+            dragListener={false}
+            className="card bg-base-100 w-96 shadow-xl fixed top-[50px] right-[50px] z-[100000]"
+          >
+            <div className="card-body p-2 rounded-sm">
+              <div className="card-actions justify-between">
+                <button
+                  className="btn btn-sm btn-outline btn-neutral btn-wide start block flex-grow"
+                  onPointerDown={startDrag}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div>
-              <Collapse title="Orginal">
-                <textarea
-                  className="text-gray-500 w-full [field-sizing:content] overflow-y-auto rounded-md text-xs font-light bg-zinc-50 p-2 border border-zinc-300 border-solid"
-                  value={selectedText}
-                />
-              </Collapse>
-              <Collapse isOpen={true}>
-                <Loader isLoading={isLoading} />
-                {!isLoading && improved && (
-                  <TypewriterEffect
-                    words={improved
-                      .split(" ")
-                      .map((word: string) => ({ text: word }))}
-                  />
-                )}
-              </Collapse>
-              <Collapse title="Changes">
-                <Loader isLoading={isLoading} />
-                <ul>
-                  {explanation &&
-                    explanation.map(
-                      ({ original, correction, explanation }, index) => (
-                        <li
-                          key={index}
-                          className="text-gray-500 w-full h-fit rounded-md text-xs font-light bg-zinc-50 p-2 border border-zinc-300 border-solid mb-4"
-                        >
-                          <p className="mb-1">
-                            <span className="font-bold">Diff : </span>
-                            <span className="text-rose-600 bg-red-200 font-bold">
-                              {original}
-                            </span>{" "}
-                            |
-                            <span className="bg-green-200 text-green-600 font-bold">
-                              {correction}
-                            </span>
-                          </p>
-                          <p>
-                            <span className="font-bold">Explanation : </span>
-                            {explanation}
-                          </p>
-                        </li>
-                      )
-                    )}
-                </ul>
-                {/* <div className="card-body p-2 rounded-sm">
+                  drag me
+                </button>
+                <button
+                  className="btn btn-sm btn-outline btn-secondary"
+                  onClick={() => {
+                    setIsOpen(false);
+                    setSelectedText("");
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <Collapse title="Orginal">
+                  <div
+                    onPointerDown={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  >
+                    <textarea
+                      className="text-gray-500 w-full [field-sizing:content] overflow-y-auto rounded-md text-xs font-light bg-zinc-50 p-2 border border-zinc-300 border-solid"
+                      value={selectedText}
+                    />
+                  </div>
+                </Collapse>
+                <Collapse isOpen={true}>
+                  <Loader isLoading={isLoading} />
+                  {!isLoading && improved && (
+                    <TypewriterEffect
+                      words={improved
+                        .split(" ")
+                        .map((word: string) => ({ text: word }))}
+                    />
+                  )}
+                </Collapse>
+                <Collapse title="Changes">
+                  <Loader isLoading={isLoading} />
+                  <ul>
+                    {explanation &&
+                      explanation.map(
+                        ({ original, correction, explanation }, index) => (
+                          <li
+                            key={index}
+                            className="text-gray-500 w-full h-fit rounded-md text-xs font-light bg-zinc-50 p-2 border border-zinc-300 border-solid mb-4"
+                          >
+                            <p className="mb-1">
+                              <span className="font-bold">Diff : </span>
+                              <span className="text-rose-600 bg-red-200 font-bold">
+                                {original}
+                              </span>{" "}
+                              |
+                              <span className="bg-green-200 text-green-600 font-bold">
+                                {correction}
+                              </span>
+                            </p>
+                            <p>
+                              <span className="font-bold">Explanation : </span>
+                              {explanation}
+                            </p>
+                          </li>
+                        )
+                      )}
+                  </ul>
+                  {/* <div className="card-body p-2 rounded-sm">
 
                 </div> */}
-              </Collapse>
+                </Collapse>
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </>
       )}
     </>
   );
--- src/pages/content/hoooks/useBackgroundComunication.tsx
+++ src/pages/content/hoooks/useBackgroundComunication.tsx
@@ -20,65 +20,65 @@ export const useBackgroundComunication = () => {
     null
   );
 
-  useEffect(() => {
-    const messageListener = (message: { action: string; data?: string }) => {
-      if (message.action !== "improveEnglish") return;
-      console.log("messageListener", message);
-      const parsedData: ImprovedTextResposne = JSON.parse(
-        message?.data as string
-      );
-      console.log({ parsedData });
-      setImprovedText(parsedData);
-      setIsLoading(false);
-    };
-    chrome.runtime.onMessage.addListener(messageListener);
-    return () => {
-      chrome.runtime.onMessage.removeListener(messageListener);
-    };
-  }, []);
-
-  useEffect(() => {
-    const openListener = (message: { action: string }) => {
-      if (message.action !== "openDrawer") return;
-      setIsOpen(true);
-      setIsLoading(true);
-    };
-    chrome.runtime.onMessage.addListener(openListener);
-    return () => {
-      chrome.runtime.onMessage.removeListener(openListener);
-    };
-  }, []);
-
-  useEffect(() => {
-    const selectionListener = (message: { action: string; data?: string }) => {
-      if (message.action !== "selectedText") return;
-      setSelectedText(message?.data as string);
-    };
-
-    chrome.runtime.onMessage.addListener(selectionListener);
-    return () => {
-      chrome.runtime.onMessage.removeListener(selectionListener);
-    };
-  }, []);
-
   // useEffect(() => {
-  //   const fetchImprovedText = async () => {
-  //     setSelectedText(dummyResponse.original);
-  //     setIsLoading(true);
-  //     const response = await fakeData();
-  //     if (response) {
-  //       console.log({ response });
-  //       const parsedData: ImprovedTextResposne = JSON.parse(response as string);
-  //       console.log({ parsedData });
-
-  //       setImprovedText(parsedData as ImprovedTextResposne);
-  //     }
+  //   const messageListener = (message: { action: string; data?: string }) => {
+  //     if (message.action !== "improveEnglish") return;
+  //     console.log("messageListener", message);
+  //     const parsedData: ImprovedTextResposne = JSON.parse(
+  //       message?.data as string
+  //     );
+  //     console.log({ parsedData });
+  //     setImprovedText(parsedData);
   //     setIsLoading(false);
   //   };
-  //   if (isOpen) {
-  //     fetchImprovedText();
-  //   }
-  // }, [isOpen]);
+  //   chrome.runtime.onMessage.addListener(messageListener);
+  //   return () => {
+  //     chrome.runtime.onMessage.removeListener(messageListener);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const openListener = (message: { action: string }) => {
+  //     if (message.action !== "openDrawer") return;
+  //     setIsOpen(true);
+  //     setIsLoading(true);
+  //   };
+  //   chrome.runtime.onMessage.addListener(openListener);
+  //   return () => {
+  //     chrome.runtime.onMessage.removeListener(openListener);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const selectionListener = (message: { action: string; data?: string }) => {
+  //     if (message.action !== "selectedText") return;
+  //     setSelectedText(message?.data as string);
+  //   };
+
+  //   chrome.runtime.onMessage.addListener(selectionListener);
+  //   return () => {
+  //     chrome.runtime.onMessage.removeListener(selectionListener);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    const fetchImprovedText = async () => {
+      setSelectedText(dummyResponse.original);
+      setIsLoading(true);
+      const response = await fakeData();
+      if (response) {
+        console.log({ response });
+        const parsedData: ImprovedTextResposne = JSON.parse(response as string);
+        console.log({ parsedData });
+
+        setImprovedText(parsedData as ImprovedTextResposne);
+      }
+      setIsLoading(false);
+    };
+    if (isOpen) {
+      fetchImprovedText();
+    }
+  }, [isOpen]);
 
   return {
     isOpen,
@@ -90,10 +90,10 @@ export const useBackgroundComunication = () => {
   };
 };
 
-// const fakeData = () => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(JSON.stringify(dummyResponse));
-//     }, 4000);
-//   });
-// };
+const fakeData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(JSON.stringify(dummyResponse));
+    }, 4000);
+  });
+};
